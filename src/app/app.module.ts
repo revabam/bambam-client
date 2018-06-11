6cd326ab3f3693ebc4c0dc5d4ce3f93b232494e6
Edel Benavides <edelbenavides@gmail.com>
2018-06-11 09:41:22 -0400

    refactored navbar and routes to match current folder structure; added navbar and favicon icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { appRoutes } from '../routes';
+import { appRoutes } from '../app/routes/routes';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './pages/template/navbar/navbar.component';
-import { DashboardComponent } from './pages/tiles/dashboard/dashboard.component';
-import { CalendarComponent } from './pages/tiles/calendar/calendar.component';
-import { CurriculumEditorComponent } from './pages/tiles/curriculum-editor/curriculum-editor.component';
-import { BoomComponent } from './pages/tiles/boom/boom.component';
+import { NavbarComponent } from './routes/template/navbar/navbar.component';
+import { DashboardComponent } from './routes/pages/dashboard/dashboard.component';
+import { CalendarComponent } from './routes/pages/calendar/calendar.component';
+import { CurriculumEditorComponent } from './routes/pages/curriculum-editor/curriculum-editor.component';
+import { BoomComponent } from './routes/pages/boom/boom.component';
 
 @NgModule({
   declarations: [
--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app/app.component';
-import { DashboardComponent } from './app/pages/tiles/dashboard/dashboard.component';
-import { CalendarComponent } from './app/pages/tiles/calendar/calendar.component';
-import { CurriculumEditorComponent } from './app/pages/tiles/curriculum-editor/curriculum-editor.component';
-import { BoomComponent } from './app/pages/tiles/boom/boom.component';
+import { AppComponent } from '../../app/app.component';
+import { DashboardComponent } from '../../app/routes/pages/dashboard/dashboard.component';
+import { CalendarComponent } from '../../app/routes/pages/calendar/calendar.component';
+import { CurriculumEditorComponent } from '../../app/routes/pages/curriculum-editor/curriculum-editor.component';
+import { BoomComponent } from '../../app/routes/pages/boom/boom.component';
 
 export const appRoutes: Routes = [
     // routes go here.
