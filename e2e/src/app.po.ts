92c41188ac4f0349b382c4a8f95473951c14969f
drewli <andrewli.wbms@gmail.com>
2018-08-09 10:09:15 -0700

    saving my personal changes to my branch

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -4,8 +4,18 @@ export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
-
+  getTitle() {
+    return browser.getTitle();
+  }
+  findElementByLinkText(link: string) {
+    return element(by.linkText(link));
+  }
+  findElementBySelector(selector: string) {
+    return element(by.css(selector));
+  }
+  /*
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+  */
 }
