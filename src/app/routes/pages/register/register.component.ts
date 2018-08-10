e2fd1f3ed58b582400a84c5f6ea7d55f108f9252
drewli <andrewli.wbms@gmail.com>
2018-08-10 12:39:33 -0700

    added register.component.ts

--- src/app/routes/pages/register/register.component.ts
+++ src/app/routes/pages/register/register.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  email: string;
+  password: string;
+  confirmPassword: string;
+  firstName: string;
+  lastName: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  register() {
+    console.log(`Registering with the following values:
+      Email: ${this.email}
+      Password: ${this.password}
+      Confirm Password: ${this.confirmPassword}
+      First Name: ${this.firstName}
+      Last Name: ${this.lastName}
+    `);
+  }
+
+  emailValid(): boolean {
+    let isValid: boolean = false;
+    // TODO
+    return isValid;
+  }
+
+  passwordValid(): boolean {
+    if (this.password !== this.confirmPassword) {
+      return false;
+    }
+    return true;
   }
 
 }
