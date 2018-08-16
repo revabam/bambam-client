d7a2772ef4be6e07c3fbd72b04beb0975a07890f
drewli <andrewli.wbms@gmail.com>
2018-08-16 07:04:42 -0700

    readding my origin changes to sub branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatAutocompleteModule, MatBadgeModule, MatBottomSheetModule, MatButtonModule, MatButtonToggleModule, MatCardModule, MatCheckboxModule, MatChipsModule, MatDatepickerModule, MatDialogModule, MatDividerModule, MatExpansionModule, MatGridListModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule, MatNativeDateModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule, MatRadioModule, MatRippleModule, MatSelectModule, MatSidenavModule, MatSliderModule, MatSlideToggleModule, MatSnackBarModule, MatSortModule, MatStepperModule, MatTableModule, MatTabsModule, MatToolbarModule, MatTooltipModule, MatTreeModule } from '@angular/material';
+import { CurriculumViewComponent } from './routes/pages/curriculum-view/curriculum-view.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
@@ -29,6 +30,7 @@ import { LocationStrategy, PathLocationStrategy } from '../../node_modules/@angu
     DashboardComponent,
     CalendarComponent,
     CurriculumEditorComponent,
+    CurriculumViewComponent,
     BoomComponent,
     LoginComponent,
     RegisterComponent,
