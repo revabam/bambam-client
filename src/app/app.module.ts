5973a98846f0617ab3acf45fb01c9e0d7c5cff49
Bradley-Walker <bradleynwalker1@gmail.com>
2018-08-16 11:57:45 -0400

    Added the dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { LoginComponent } from './routes/pages/login/login.component';
 import { RegisterComponent } from './routes/pages/register/register.component';
 import { NavbarComponent } from './routes/template/navbar/navbar.component';
 import { UserService } from './services/user.service';
+import { BatchService } from './services/batch/batch.service';
 import { LocationStrategy, PathLocationStrategy } from '../../node_modules/@angular/common';
 
 
@@ -79,6 +80,7 @@ import { LocationStrategy, PathLocationStrategy } from '../../node_modules/@angu
   ],
   providers: [
     UserService,
+    BatchService,
     {provide: LocationStrategy, useClass: PathLocationStrategy}
   ],
   bootstrap: [AppComponent]
