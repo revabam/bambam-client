836780aeca450a450978f30975e3438ccc1f0463
wsingleton <wezley.singleton@gmail.com>
2018-08-16 01:42:22 -0400

    specifying path location routing strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,61 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { appRoutes } from '../app/routes/routes';
 import { FormsModule } from '@angular/forms';
-
+import { MatAutocompleteModule, MatBadgeModule, MatBottomSheetModule, MatButtonModule, MatButtonToggleModule, MatCardModule, MatCheckboxModule, MatChipsModule, MatDatepickerModule, MatDialogModule, MatDividerModule, MatExpansionModule, MatGridListModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule, MatNativeDateModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule, MatRadioModule, MatRippleModule, MatSelectModule, MatSidenavModule, MatSliderModule, MatSlideToggleModule, MatSnackBarModule, MatSortModule, MatStepperModule, MatTableModule, MatTabsModule, MatToolbarModule, MatTooltipModule, MatTreeModule } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { TimepickerModule } from 'ngx-bootstrap/timepicker';
-
+import { appRoutes } from '../app/routes/routes';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './routes/template/navbar/navbar.component';
-import { DashboardComponent } from './routes/pages/dashboard/dashboard.component';
+import { BoomComponent } from './routes/pages/boom/boom.component';
 import { CalendarComponent } from './routes/pages/calendar/calendar.component';
 import { CurriculumEditorComponent } from './routes/pages/curriculum-editor/curriculum-editor.component';
-import { BoomComponent } from './routes/pages/boom/boom.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import {
-  MatAutocompleteModule,
-  MatBadgeModule,
-  MatBottomSheetModule,
-  MatButtonModule,
-  MatButtonToggleModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatChipsModule,
-  MatDatepickerModule,
-  MatDialogModule,
-  MatDividerModule,
-  MatExpansionModule,
-  MatGridListModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatMenuModule,
-  MatNativeDateModule,
-  MatPaginatorModule,
-  MatProgressBarModule,
-  MatProgressSpinnerModule,
-  MatRadioModule,
-  MatRippleModule,
-  MatSelectModule,
-  MatSidenavModule,
-  MatSliderModule,
-  MatSlideToggleModule,
-  MatSnackBarModule,
-  MatSortModule,
-  MatStepperModule,
-  MatTableModule,
-  MatTabsModule,
-  MatToolbarModule,
-  MatTooltipModule,
-  MatTreeModule,
-} from '@angular/material';
+import { DashboardComponent } from './routes/pages/dashboard/dashboard.component';
 import { LoginComponent } from './routes/pages/login/login.component';
-import { UserService } from './services/user.service';
-import { HttpClient, HttpHandler, HttpClientModule } from '@angular/common/http';
 import { RegisterComponent } from './routes/pages/register/register.component';
+import { NavbarComponent } from './routes/template/navbar/navbar.component';
+import { UserService } from './services/user.service';
+import { LocationStrategy, PathLocationStrategy } from '../../node_modules/@angular/common';
+
+
+
 
 @NgModule({
   declarations: [
@@ -113,7 +78,8 @@ import { RegisterComponent } from './routes/pages/register/register.component';
     BrowserAnimationsModule
   ],
   providers: [
-    UserService
+    UserService,
+    {provide: LocationStrategy, useClass: PathLocationStrategy}
   ],
   bootstrap: [AppComponent]
 })
