545a4a8a7df89e1b15c63feaf693d27ea61e4f72
Bradley-Walker <bradleynwalker1@gmail.com>
2018-08-16 14:06:26 -0400

    pushing so everyone can see

---
+++ src/app/routes/routes.module.ts
@@ -0,0 +1,58 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
+import { CurriculumEditorComponent } from './pages/curriculum-editor/curriculum-editor.component';
+import { BoomComponent } from './pages/boom/boom.component';
+
+const ROUTES: Routes = [
+  {
+      path: 'dashboard',
+      component: DashboardComponent
+  },
+
+  {
+      path: 'login',
+      component: LoginComponent
+  },
+
+  {
+      path: 'register',
+      component: RegisterComponent
+  },
+
+  {
+      path: 'calendar',
+      component: CalendarComponent
+  },
+
+  {
+      path: 'curriculum/editor',
+      component: CurriculumEditorComponent
+  },
+
+  {
+      path: 'boom',
+      component: BoomComponent
+  },
+  {
+      path: '',
+      pathMatch: 'full',
+      redirectTo: 'dashboard'
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(ROUTES, {onSameUrlNavigation: 'reload'})
+  ],
+  declarations: [],
+  exports: [
+    RouterModule
+  ]
+})
+export class RoutesModule { }
