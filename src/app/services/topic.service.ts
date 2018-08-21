03095c8cf89689f9bdbfb830ec5c418b9f30b354
khaleelwilliams <khaleelwilliams92@gmail.com>
2018-08-21 15:04:56 -0400

    added ability to edit profile

--- src/app/services/topic.service.ts
+++ src/app/services/topic.service.ts
@@ -40,6 +40,7 @@ export class TopicService {
   getTopicById(id: number): Observable<Topic> {
     console.log('[LOG] - In TopicService.getTopicById()');
     return this.http.get<Topic>(environment.apiUrl + `topics/${id}`, HTTP_OPTIONS);
+  }
   /**
    * The function used to deactivate a topic in the server
    */
