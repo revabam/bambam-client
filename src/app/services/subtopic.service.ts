1530c66c9b395de79e6a460e1d832df0c7df13b8
drewli <andrewli.wbms@gmail.com>
2018-08-21 08:31:39 -0700

    readding the file

---
+++ src/app/services/subtopic.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+/*
+ * HttpClient - What we use to make the http request.
+ * HttpHeaders - What we need to pass in to the http request
+ * in order to define the Content-Type.
+ * Subtopic - The model that the current service pertains to.
+ * Observable - The type of object returned by the http request,
+ * in order to enable us to get the response asynchronously.
+ * Environment - Contains the server endpoint that we're making
+ * the HTTP Request to.
+ */
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Subtopic } from '../models/subtopic';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+// The content type for our HTTP requests is JSON.
+const HTTP_OPTIONS = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubtopicService {
+
+  // The dependency to be injected, in order to use an HttpClient.
+  constructor(private http: HttpClient) { }
+
+  /**
+   * The function used to fetch all the subtopics from the server.
+   */
+  getAll(): Observable<Subtopic[]> {
+    console.log('[LOG] - In SubtopicService.getAll()');
+    return this.http.get<Subtopic[]>(environment.apiUrl + 'subtopics', HTTP_OPTIONS);
+  }
+
+  getSubtopicByParentId(id: number): Observable<Subtopic[]> {
+    console.log('[LOG} - In SubtopicService.getSubtopicByParentId()');
+    return this.http.get<Subtopic[]>(environment.apiUrl + `subtopics?parentTopic_id=${id}`, HTTP_OPTIONS);
+  }
+}
