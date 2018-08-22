f3552953938da71d61a1fbb806cd44257b248e7d
Aaron Mathews <aaron.mathews95@gmail.com>
2018-08-22 16:38:23 -0400

    final changes

--- src/app/services/calendar.service.ts
+++ src/app/services/calendar.service.ts
@@ -54,8 +54,8 @@ export class CalendarService {
     return this.http.post<CalendarEvent>(environment.apiUrl + 'calendar-events', json, HTTP_OPTIONS);
   }
 
-  getCalendarEvents(): Observable<MyEvent[]> {
-    return this.http.get<MyEvent[]>(environment.apiUrl + 'calendar-events', HTTP_OPTIONS);
+  getCalendarEvents(): Observable<CalendarEvent[]> {
+    return this.http.get<CalendarEvent[]>(environment.apiUrl + 'calendar-events', HTTP_OPTIONS);
   }
 
   getCalendarSubtopics(): Observable<Subtopic[]> {
