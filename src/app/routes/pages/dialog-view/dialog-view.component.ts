3232338311494488a7d48b5a32ca8239c471fc51
khaleelwilliams <khaleelwilliams92@gmail.com>
2018-08-24 10:38:59 -0400

    documentation on dialog-view-component

--- src/app/routes/pages/dialog-view/dialog-view.component.ts
+++ src/app/routes/pages/dialog-view/dialog-view.component.ts
@@ -4,16 +4,26 @@ import { TopicService } from '../../../services/topic.service';
 import { SubtopicService } from '../../../services/subtopic.service';
 import { Topic } from '../../../models/topic';
 
+/**
+ * Component created to inject a dialog view whenever adding Topics or Sub-Topics in the
+ * Curriculum editor.
+ * Need to convert services from a field in data to be injected into the constructor.
+ * @author Karen Matney | Khaleel Williams | Andrew Li | 1806-Jun18-USF-Java | Wezley Singleton
+*/
 @Component({
   selector: 'app-dialog-view',
   templateUrl: './dialog-view.component.html',
   styleUrls: ['./dialog-view.component.css']
 })
 export class DialogViewComponent implements OnInit {
+
   topicName: string;
   subtopicName: string;
+  // Switches visibility of Sub-topic input field
   fieldVisible = false;
+  // List of subtopic to be added
   subtopics: string[] = [];
+  // List of all available topics to be selected whenever adding a new sub-topic.
   dropDownTopics: Topic[] = [];
 
   constructor(
@@ -26,10 +36,17 @@ export class DialogViewComponent implements OnInit {
     });
   }
 
+  /**
+   * Exits dialog view whenver cancel or ok is hit in the dialog view.
+   */
   close(): void {
     this.dialogRef.close();
   }
 
+  /**
+   * Adds user created topics from the input fields provided by the dialog view.
+   * @author Karen Matney | Khaleel Williams
+   */
   addTopic(): void {
     console.log('[DEBUG] - in addTopic');
     console.log(`${this.topicName} - ${this.subtopicName}`);
@@ -64,10 +81,18 @@ export class DialogViewComponent implements OnInit {
     });
   }
 
+  /**
+   * Makes Sub-Topic input field visible when pressing the addition button.
+   * @author Karen Matney
+   */
   makefieldVisible() {
     this.fieldVisible = true;
   }
 
+  /**
+   * Adds a list of Sub-Topics to a topic.
+   * @author Karen Matney
+   */
   addToSubTopics(): void {
     if (this.subtopicName !== this.data['subtopicService'].reactivateName(this.subtopicName)) {
       alert('Invalid Subtopic Name');
