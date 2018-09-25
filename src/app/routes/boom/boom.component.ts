f95adce82f8f239d3fef5ceda57477dc219b92a1
richardiskra <richard.iskra@valpo.edu>
2018-09-25 13:57:43 -0400

    cleaned up code and added ngDoc

--- src/app/routes/boom/boom.component.ts
+++ src/app/routes/boom/boom.component.ts
@@ -8,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class BoomComponent implements OnInit {
 
   data = {
-    focus: "full-stack java", curriculum: [{ week: "week 1", tasks: [{ name: "html", status: 1 }, { name: "css", status: 1 }, { name: "js", status: 1 }] },
-    { week: "week 2", tasks: [{ name: "java", status: 1 }, { name: "hibernate", status: 2 }, { name: "spring", status: 1 }, { name: "aws", status: 1 }] },
-    { week: "week 3", tasks: [{ name: "ts", status: 1 }, { name: "angular", status: 2 }, { name: "sql", status: 1 }, { name: "dev-ops", status: 2 }] }]
+    focus: 'full-stack java', curriculum:
+    [{ week: 'week 1', tasks: [{ name: 'html', status: 1 }, { name: 'css', status: 1 }, { name: 'js', status: 1 }] },
+    { week: 'week 2', tasks: [{ name: 'java', status: 1 }, { name: 'hibernate', status: 2 },
+        { name: 'spring', status: 1 }, { name: 'aws', status: 1 }] },
+    { week: 'week 3', tasks: [{ name: 'ts', status: 1 }, { name: 'angular', status: 2 },
+        { name: 'sql', status: 1 }, { name: 'dev-ops', status: 2 }] }]
   };
 
   completed = [];
@@ -24,8 +27,8 @@ export class BoomComponent implements OnInit {
     responsive: true
   };
   public barChartLabels: string[] = this.weeks;
-  public barChartType: string = 'bar';
-  public barChartLegend: boolean = true;
+  public barChartType = 'bar';
+  public barChartLegend = true;
 
   public barChartData: any[] = [
     { data: this.completed, label: 'Completed' },
@@ -33,18 +36,22 @@ export class BoomComponent implements OnInit {
   ];
 
 
-//ponut chart
+  // ponut chart
   public doughnutChartLabels: string[] = ['Completed', 'Missed'];
   public doughnutChartData: number[] = this.getProgres();
-  public doughnutChartType: string = 'doughnut';
+  public doughnutChartType = 'doughnut';
 
+
+  /**
+   * determines how many tasks have been completed and how many have been missed
+   */
   getProgres() {
-    var prog: number[] = [0, 0];
-    for (var i = 0; i < this.data.curriculum.length; i++) {
-      for (var j = 0; j < this.data.curriculum[i].tasks.length; j++) {
-        if (this.data.curriculum[i].tasks[j].status == 1) {
+    const prog: number[] = [0, 0];
+    for (let i = 0; i < this.data.curriculum.length; i++) {
+      for (let j = 0; j < this.data.curriculum[i].tasks.length; j++) {
+        if (this.data.curriculum[i].tasks[j].status === 1) {
           ++prog[0];
-        } else if (this.data.curriculum[i].tasks[j].status == 2) {
+        } else if (this.data.curriculum[i].tasks[j].status === 2) {
           ++prog[1];
         }
       }
@@ -53,17 +60,17 @@ export class BoomComponent implements OnInit {
   }
 
   ngOnInit() {
-    var completed = 0;
-    var missed = 0;
-    for(var i = 0; i < this.data.curriculum.length; i++){
-      //get week titles for the axis lables
+    let completed = 0;
+    let missed = 0;
+    for (let i = 0; i < this.data.curriculum.length; i++) {
+      // get week titles for the axis lables
       this.weeks.push(this.data.curriculum[i].week);
 
-      //get the completed and missed takss and count how many there are each week
-      for(var j = 0; j < this.data.curriculum[i].tasks.length; j++){
-        if(this.data.curriculum[i].tasks[j].status == 1){
+      // get the completed and missed takss and count how many there are each week
+      for (let j = 0; j < this.data.curriculum[i].tasks.length; j++) {
+        if (this.data.curriculum[i].tasks[j].status === 1) {
           ++completed;
-        } else if (this.data.curriculum[i].tasks[j].status == 2){
+        } else if (this.data.curriculum[i].tasks[j].status === 2) {
           ++missed;
         }
       }
