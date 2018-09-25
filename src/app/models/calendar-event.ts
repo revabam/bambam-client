355ed4191ac6a78249550232959d8f78563c1c61
MarcinSalamon <Marcin+salamonmarcin29@gmail.com>
2018-09-25 12:00:46 -0400

    ngDocs for calendar-event model

--- src/app/models/calendar-event.ts
+++ src/app/models/calendar-event.ts
@@ -1,3 +1,9 @@
+/**
+ * calendar event model that can be persisted to the database
+ * not to be confused with the CalendarEvent interface that is used to populate the MaterialCalendar
+ *
+ * @author Marcin Salamon | Spark1806-USF-Java | Steven Kelsey
+ */
 export class CalendarEvent {
     id?: number;
     title: string;
