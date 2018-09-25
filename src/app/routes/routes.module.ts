61da1e1a82a1e7f6a6bb77b89928d1150f9cb620
MarcinSalamon <Marcin+salamonmarcin29@gmail.com>
2018-09-25 10:25:54 -0400

    fixed git merge annotations

--- src/app/routes/routes.module.ts
+++ src/app/routes/routes.module.ts
@@ -1,27 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { CalendarComponent } from './pages/calendar/calendar.component';
-import { CurriculumEditorComponent } from './pages/curriculum-editor/curriculum-editor.component';
-import { BoomComponent } from './pages/boom/boom.component';
-import { CreateVersionComponent } from './pages/create-version/create-version.component';
-import { DialogViewComponent } from './pages/dialog-view/dialog-view.component';
-import { EventDuplicateModalComponent } from './pages/calendar/event-duplicate-modal/event-duplicate-modal.component';
-import { CalendarModalComponent } from './pages/calendar/calendar-modal/calendar-modal.component';
-=======
-import { DashboardComponent } from '../routes/dashboard/dashboard.component';
-import { CalendarComponent } from '../routes/calendar/calendar.component';
-import { CreateVersionComponent } from '../routes/curriculum-editor/create-version/create-version.component';
-import { CurriculumEditorComponent } from '../routes/curriculum-editor/curriculum-editor.component';
-import { BoomComponent } from '../routes/boom/boom.component';
-import { LoginComponent } from '../routes/login/login.component';
-import { RegisterComponent } from '../routes/register/register.component';
-import { DialogViewComponent } from '../routes/dialog-view/dialog-view.component';
->>>>>>> 499be7f5a3c51579f8911c996114e466f6ab9acb
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { CurriculumEditorComponent } from './curriculum-editor/curriculum-editor.component';
+import { BoomComponent } from './boom/boom.component';
+import { DialogViewComponent } from './dialog-view/dialog-view.component';
+import { CreateVersionComponent } from './curriculum-editor/create-version/create-version.component';
+import { EventDuplicateModalComponent } from './calendar/event-duplicate-modal/event-duplicate-modal.component';
+import { CalendarModalComponent } from './calendar/calendar-modal/calendar-modal.component';
 
 const ROUTES: Routes = [
   {
