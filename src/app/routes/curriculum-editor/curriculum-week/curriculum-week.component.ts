12a457a5686e2b26e68d02b2e37f8f8124d0868c
chin24 <chin2413@gmail.com>
2018-09-27 17:24:28 -0400

    added docs

--- src/app/routes/curriculum-editor/curriculum-week/curriculum-week.component.ts
+++ src/app/routes/curriculum-editor/curriculum-week/curriculum-week.component.ts
@@ -1,6 +1,9 @@
 import { CurriculumWeek } from './../../../models/curriculum-week';
 import { Component, OnInit, Input } from '@angular/core';
 
+/**
+ * Allows user to a curriculums week.
+ */
 @Component({
   selector: 'app-curriculum-week',
   templateUrl: './curriculum-week.component.html',
