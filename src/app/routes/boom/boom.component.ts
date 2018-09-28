a5aad401be3d9db22249cf716c1678f377632a55
richardiskra <richard.iskra@valpo.edu>
2018-09-28 13:16:38 -0400

    bar chart now tracks canceled tasks. pie chart labels changed

--- src/app/routes/boom/boom.component.ts
+++ src/app/routes/boom/boom.component.ts
@@ -9,36 +9,39 @@ export class BoomComponent implements OnInit {
 
   data = [{
     title: '1806Spark Jun25 Java - Steven', id: 1, curriculum:
-      [{ week: 'week 1', tasks: [{ name: 'html', status: 1 }, { name: 'css', status: 1 }, { name: 'js', status: 1 }] },
+      [{ week: 'week 1', tasks: [{ name: 'html', status: 1 }, { name: 'css', status: 1 },
+                                 { name: 'js', status: 1 }, { name: 'mongoDb', status: 3 }] },
       {
         week: 'week 2', tasks: [{ name: 'java', status: 1 }, { name: 'hibernate', status: 1 },
-        { name: 'spring', status: 2 }, { name: 'aws', status: 1 }]
+                                { name: 'spring', status: 2 }, { name: 'aws', status: 1 }]
       },
       {
         week: 'week 3', tasks: [{ name: 'ts', status: 1 }, { name: 'angular', status: 1 },
-        { name: 'sql', status: 1 }, { name: 'dev-ops', status: 1 }]
+                                { name: 'sql', status: 1 }, { name: 'dev-ops', status: 2 }]
       }, { week: 'week 4', tasks: [] }]
   },
   {
     title: '1802 Feb05 Java - August', id: 2, curriculum:
-      [{ week: 'week 1', tasks: [{ name: 'html', status: 1 }, { name: 'css', status: 1 }, { name: 'js', status: 1 }] },
+      [{ week: 'week 1', tasks: [{ name: 'html', status: 1 }, { name: 'css', status: 1 },
+                                 { name: 'js', status: 1 }, { name: 'mongoDb', status: 2 }] },
       {
-        week: 'week 2', tasks: [{ name: 'java', status: 1 }, { name: 'hibernate', status: 2 },
-        { name: 'spring', status: 1 }, { name: 'aws', status: 1 }]
+        week: 'week 2', tasks: [{ name: 'java', status: 1 }, { name: 'hibernate', status: 3 },
+                                { name: 'spring', status: 1 }, { name: 'aws', status: 1 }]
       },
       {
         week: 'week 3', tasks: [{ name: 'ts', status: 1 }, { name: 'angular', status: 2 },
-        { name: 'sql', status: 1 }, { name: 'dev-ops', status: 2 }]
+                                { name: 'sql', status: 1 }, { name: 'dev-ops', status: 3 }]
       },
       {
         week: 'week 4', tasks: [{ name: 'stuff', status: 1 }, { name: 'more stuff', status: 2 },
-        { name: 'even more stuff', status: 1 }, { name: 'yet even more stuff :O', status: 1 }]
+                                { name: 'even more stuff', status: 3 }, { name: 'yet even more stuff :O', status: 1 }]
       }]
   }
   ];
 
   completed: number[] = [];
   missed: number[] = [];
+  canceled: number[] = [];
   weeks: string[] = [];
 
   constructor() { }
@@ -54,7 +57,8 @@ export class BoomComponent implements OnInit {
 
   public barChartData: any[] = [
     { data: [], label: 'Completed' },
-    { data: [], label: 'Missed' }
+    { data: [], label: 'Missed' },
+    { data: [], label: 'Canceled' }
   ];
 
   public barColors: Array<any> = [
@@ -69,10 +73,16 @@ export class BoomComponent implements OnInit {
       backgroundColor: 'yellow',
       borderColor: 'rgba(255,255,255,1)',
       pointBorderColor: 'rgba(255,255,255,1)',
+    },
+    {
+      // canceled color
+      backgroundColor: 'red',
+      borderColor: 'rgba(255,255,255,1)',
+      pointBorderColor: 'rgba(255,255,255,1)',
     }];
 
   // donut chart
-  public doughnutChartLabels: string[] = ['Completed', 'Missed'];
+  public doughnutChartLabels: string[] = ['Percentile rached', 'Under percentile'];
   public doughnutChartData: number[] = [this.data.length, 0];
   public doughnutChartType = 'doughnut';
   public donutColors = [{ backgroundColor: ['green', 'yellow'] }];
@@ -83,6 +93,7 @@ export class BoomComponent implements OnInit {
   getWeeklyProgress(curriculumId) {
     this.completed = [];
     this.missed = [];
+    this.canceled = [];
     this.weeks = [];
 
     // get week titles for the axis lables
@@ -95,24 +106,30 @@ export class BoomComponent implements OnInit {
     for (let i = 0; i < this.data[curriculumId].curriculum.length; i++) {
       let completed = 0;
       let missed = 0;
+      let canceled = 0;
       for (let j = 0; j < this.data[curriculumId].curriculum[i].tasks.length; j++) {
         if (this.data[curriculumId].curriculum[i].tasks[j].status === 1) {
           ++completed;
         } else if (this.data[curriculumId].curriculum[i].tasks[j].status === 2) {
           ++missed;
+        } else if (this.data[curriculumId].curriculum[i].tasks[j].status === 3) {
+          ++canceled;
         }
       }
       this.completed.push(completed);
       this.missed.push(missed);
+      this.canceled.push(canceled);
     }
+    // update the data
     const clone = JSON.parse(JSON.stringify(this.barChartData));
     clone[0].data = this.completed;
     clone[1].data = this.missed;
+    clone[2].data = this.canceled;
     this.barChartData = clone;
   }
 
   /**
-   * Find the selected bacth curriculum
+   * Find the selected batch curriculum
    */
   selectedCurriculum(event) {
     const id = event.value;
@@ -144,15 +161,13 @@ export class BoomComponent implements OnInit {
           length++;
         }
       }
-      console.log(completed / length);
-      console.log(percentile);
       if (completed / length >= percentile) {
         ++prog[0];
       } else {
         ++prog[1];
       }
     }
-    console.log(prog);
+    // update the data
     let clone2 = JSON.parse(JSON.stringify(this.doughnutChartData));
     clone2 = prog;
     this.doughnutChartData = clone2;
@@ -164,5 +179,5 @@ export class BoomComponent implements OnInit {
 }
 
 /**
- * @author Richard Iskra | Obosa Nosa Igeibaro | Eddie Grays
+ * @author Richard Iskra | Obosa Nosa-Igiebor | Eddie Grays
  */
