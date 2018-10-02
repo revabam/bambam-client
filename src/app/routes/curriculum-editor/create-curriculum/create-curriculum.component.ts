ae83a59951636bfccad248c3fcf74babba63cd16
Chinedu Ozodi <chinedu.ozodi@gmail.com>
2018-10-02 00:01:44 -0400

    fixed creation of new curriculum using recursion

--- src/app/routes/curriculum-editor/create-curriculum/create-curriculum.component.ts
+++ src/app/routes/curriculum-editor/create-curriculum/create-curriculum.component.ts
@@ -41,7 +41,7 @@ export class CreateCurriculumComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) public data: object,
     private curriculumDayService: CurriculumDayService,
     private curriculumWeekService: CurriculumWeekService
-    ) { }
+  ) { }
 
   ngOnInit() {
   }
@@ -75,9 +75,8 @@ export class CreateCurriculumComponent implements OnInit {
      * an http post. Also, if the post is successful, we
      * push it to the parent component's curriculum array
      * so that the user can see the changes.
-     * @author - Chinedu Ozodi | 1806-Sep-18-USF-Java | Steven Kelsey
      */
-    this.data['curriculumService'].post(newCurriculum).subscribe(curr => {
+    this.data['curriculumService'].post(newCurriculum).subscribe( (curr: Curriculum) => {
       const newVersion = curr.version;
       let nameNum = 1;
       for (let i = 0; i < this.data['curriculums'].length; i++) {
@@ -93,28 +92,10 @@ export class CreateCurriculumComponent implements OnInit {
       if (curr !== undefined && curr !== null) {
         this.data['curriculums'].push(curr);
 
-        // add weeks to the curriculum
-        for (let i = 0; i < this.numberOfWeeks; i++) {
-          // Creates the weeks here
-          const week: CurriculumWeek = {
-            curriculumId: curr.id,
-            weekNum: i + 1
-          };
-          this.curriculumWeekService.post(week).subscribe( (savedWeek) => {
-            console.log('saved week');
-            console.log(savedWeek);
-
-            for (let d = 1; d < 6; d++) {
-              // Creates the new day, numbers 2 - 5 for Monday through Friday
-              const day: CurriculumDay = {
-                dayNum: d,
-                weekId: savedWeek.id,
-                daySubTopics: []
-              };
-              this.curriculumDayService.post(day).subscribe( (savedDay) => {
-              });
-            }
-          });
+        // save weeks
+        if (curr.numberOfWeeks > 0) {
+          // save the first week, start from week 1
+          this.saveWeek(curr, 1);
         }
 
       } else {
@@ -125,6 +106,54 @@ export class CreateCurriculumComponent implements OnInit {
     // After the curriculum is added, we're done with the modal.
     this.close();
   }
+
+  /**
+   * Creates and persists a week for the given curriculum
+   * @param curriculum curriculum to save to
+   * @param weekNum weekNum for week to create and save
+   * @author - Chinedu Ozodi | 1806-Sep-18-USF-Java | Steven Kelsey
+   */
+  saveWeek(curriculum: Curriculum, weekNum: number) {
+    // Creates the weeks here
+    const week: CurriculumWeek = {
+      curriculumId: curriculum.id,
+      weekNum: weekNum
+    };
+    this.curriculumWeekService.post(week).subscribe((savedWeek) => {
+      // creates and persists the days for the week starting from 1 (Monday)
+      this.saveDay(curriculum, savedWeek, 1);
+    });
+  }
+
+  /**
+   * Creates and persists days for a given week
+   * @param curriculum curriculum to save to
+   * @param week week to save to
+   * @param index index/day to create and save
+   * @author - Chinedu Ozodi | 1806-Sep-18-USF-Java | Steven Kelsey
+   */
+  saveDay(curriculum: Curriculum, week: CurriculumWeek, index: number) {
+    const day: CurriculumDay = {
+      dayNum: index,
+      weekId: week.id,
+      daySubTopics: []
+    };
+    this.curriculumDayService.post(day).subscribe(() => {
+      // make sure there is only 5 days in a work week
+      if (index < 5) {
+        index++;
+        this.saveDay(curriculum, week, index);
+      } else {
+        // check to see if it needs more weeks to be created
+        if (week.weekNum < curriculum.numberOfWeeks) {
+          // Save the next week
+          const weekNum = week.weekNum + 1;
+          this.saveWeek(curriculum, weekNum);
+        }
+      }
+    });
+  }
+
   // here we are performing validation for the curriculum entered
   //    the submit button is disabled until both name and weeks are entered correctly
   validate() {
