11ee2ff2f272c05b002c42a4e01e754837b957e4
Joe Shannon <prjctmedia@USF-Gold-Wifi-226-114-193.laptops.usf.edu>
2018-10-02 10:33:18 -0400

    merging

--- src/app/routes/curriculum-editor/create-curriculum/create-curriculum.component.ts
+++ src/app/routes/curriculum-editor/create-curriculum/create-curriculum.component.ts
@@ -26,7 +26,14 @@ export class CreateCurriculumComponent implements OnInit {
    */
   constructor(
     public dialogRef: MatDialogRef<CreateVersionComponent>,
+<<<<<<< HEAD
     @Inject(MAT_DIALOG_DATA) public data: object) { }
+=======
+    @Inject(MAT_DIALOG_DATA) public data: object,
+    private curriculumDayService: CurriculumDayService,
+    private curriculumWeekService: CurriculumWeekService
+  ) { }
+>>>>>>> 9c40b316aa93edd62f8becd30abc9a1226efc871
 
   ngOnInit() {
   }
@@ -85,9 +92,35 @@ export class CreateCurriculumComponent implements OnInit {
      * push it to the parent component's curriculum array
      * so that the user can see the changes.
      */
+<<<<<<< HEAD
     this.data['curriculumService'].post(newCurriculum).subscribe(curr => {
       if (curr !== undefined && curr !== null) {
         this.data['curriculums'].push(curr);
+=======
+    this.data['curriculumService'].post(newCurriculum).subscribe( (curr: Curriculum) => {
+      const newVersion = curr.version;
+      let nameNum = 1;
+      for (let i = 0; i < this.data['curriculums'].length; i++) {
+        if (this.data['curriculums'][i].name === newCurriculum.name) {
+          if (this.data['curriculums'][i].version >= nameNum) {
+            ++nameNum;
+          } else {
+            continue;
+          }
+        }
+      }
+
+      curr.version = nameNum;
+      if (curr !== undefined && curr !== null) {
+        this.data['curriculums'].push(curr);
+
+        // save weeks
+        if (curr.numberOfWeeks > 0) {
+          // save the first week, start from week 1
+          this.saveWeek(curr, 1);
+        }
+
+>>>>>>> 9c40b316aa93edd62f8becd30abc9a1226efc871
       } else {
         console.log('returned null');
         this.data['curriculums'].push(newCurriculum);
@@ -100,4 +133,73 @@ export class CreateCurriculumComponent implements OnInit {
     // After the curriculum is added, we're done with the modal.
     this.close();
   }
+<<<<<<< HEAD
+=======
+
+  /**
+   * Creates and persists a week for the given curriculum
+   * @param curriculum curriculum to save to
+   * @param weekNum weekNum for week to create and save
+   * @author - Chinedu Ozodi | 1806-Sep-18-USF-Java | Steven Kelsey
+   */
+  saveWeek(curriculum: Curriculum, weekNum: number) {
+    // Creates the weeks here
+    const week: CurriculumWeek = {
+      curriculumId: curriculum.id,
+      weekNum: weekNum
+    };
+    this.curriculumWeekService.post(week).subscribe((savedWeek) => {
+      // creates and persists the days for the week starting from 1 (Monday)
+      this.saveDay(curriculum, savedWeek, 1);
+    });
+  }
+
+  /**
+   * Creates and persists days for a given week
+   * @param curriculum curriculum to save to
+   * @param week week to save to
+   * @param index index/day to create and save
+   * @author - Chinedu Ozodi | 1806-Sep-18-USF-Java | Steven Kelsey
+   */
+  saveDay(curriculum: Curriculum, week: CurriculumWeek, index: number) {
+    const day: CurriculumDay = {
+      dayNum: index,
+      weekId: week.id,
+      daySubTopics: []
+    };
+    this.curriculumDayService.post(day).subscribe(() => {
+      // make sure there is only 5 days in a work week
+      if (index < 5) {
+        index++;
+        this.saveDay(curriculum, week, index);
+      } else {
+        // check to see if it needs more weeks to be created
+        if (week.weekNum < curriculum.numberOfWeeks) {
+          // Save the next week
+          const weekNum = week.weekNum + 1;
+          this.saveWeek(curriculum, weekNum);
+        }
+      }
+    });
+  }
+
+  // here we are performing validation for the curriculum entered
+  //    the submit button is disabled until both name and weeks are entered correctly
+  validate() {
+    this.isValid = true;
+    // validating user input
+    if (!this.curriculumName || !this.numberOfWeeks) {
+      this.isValid = false;
+    } else {
+      // if curriculum name is valid
+      if (this.curriculumName.length < 2) {
+        this.isValid = false;
+      }
+      // if curriculum weeks is valid
+      if (this.numberOfWeeks < 1) {
+        this.isValid = false;
+      }
+    }
+  }
+>>>>>>> 9c40b316aa93edd62f8becd30abc9a1226efc871
 }
