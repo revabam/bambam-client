60613eb6560510c6dac1599177318936bca1f209
Chinedu Ozodi <chin2413@gmail.com>
2018-10-03 22:35:12 -0400

    Revert "Feature/show tabs"

--- src/app/routes/curriculum-editor/curriculum-editor.component.ts
+++ src/app/routes/curriculum-editor/curriculum-editor.component.ts
@@ -3,7 +3,7 @@ import { DaySubtopicService } from './../../services/day-subtopic.service';
 import { CurriculumWeekService } from './../../services/curriculum-week.service';
 import { CurriculumDay } from './../../models/curriculum-day';
 import { CreateVersionComponent } from '../../routes/curriculum-editor/create-version/create-version.component';
-import { Component, OnInit, ViewEncapsulation} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CurriculumService } from '../../services/curriculum.service';
 import { Curriculum } from '../../models/curriculum';
 import { MatDialog } from '@angular/material';
@@ -18,8 +18,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-curriculum-editor',
   templateUrl: './curriculum-editor.component.html',
-  styleUrls: ['./curriculum-editor.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./curriculum-editor.scss']
 })
 export class CurriculumEditorComponent implements OnInit {
   // Arrays of all the elements we're fetching from the server.
@@ -27,8 +26,6 @@ export class CurriculumEditorComponent implements OnInit {
   curriculumNames: string[] = [];
 
   selectedCurriculum: Curriculum;
-
-  background = '';
 
   /**
    * @param curriculumService - The service (defined by us)
@@ -65,7 +62,6 @@ export class CurriculumEditorComponent implements OnInit {
       this.cognito.bamUser = JSON.parse(sessionStorage.getItem('user'));
     }
     this.getAllCurriculums();
-    this.toggleBackground();
   }
 
   /**
@@ -184,14 +180,6 @@ export class CurriculumEditorComponent implements OnInit {
       }
     }
     return curriculum;
-  }
-
-  /**
-   * Method to set the background color on the tabs.
-   * @author - Alex Moraga | 1806-Sep-18-USF-Java | Steven Kelsey
-   */
-  toggleBackground() {
-    this.background = this.background ? '' : 'primary';
   }
 
   /**
